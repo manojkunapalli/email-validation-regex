f42da9540c7c052222482bb242fef5cab3302a63
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 01:44:11 +0530

    Added: validate all optional part

--- validateEmailRegex.sh
+++ validateEmailRegex.sh
@@ -2,7 +2,7 @@
 
 echo "welcome to Email Validation"
 
-pattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
+pattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 
 read -p "Enter Email Id = " email
 
