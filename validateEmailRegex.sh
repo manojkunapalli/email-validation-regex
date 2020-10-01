145188a4aecc683f0589d5c73b36672287bf7034
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 01:16:17 +0530

    Added: validate first mandatory part

--- validateEmailRegex.sh
+++ validateEmailRegex.sh
@@ -2,3 +2,13 @@
 
 echo "welcome to Email Validation"
 
+pattern="^[0-9a-zA-Z]"
+
+read -p "Enter Email Id = " email
+
+if [[ $email =~ $pattern ]]
+then
+	echo "given email id is valid";
+else
+	echo "email id is not valid"
+fi
